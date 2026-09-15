be9fb0b0bc48532349bd33a03539cf3645fd8a42
aitoreta43-lang <aitoreta43@gmail.com>
2026-09-15 23:17:41 +0530

    bot: add notification nmu2yslmpzm9

--- js/data.js
+++ js/data.js
@@ -1960,7 +1960,13 @@ const POSTS = [
 
 const TESTIMONIALS = [];
 
-const NOTIFICATIONS = [];
+const NOTIFICATIONS = [
+  {
+    "id": "nmu2yslmpzm9",
+    "text": "List Your Bnb With Us\nGet More Happy Clients",
+    "link": "https://wa.me/qr/PARIBLU7GPSO01"
+  }
+];
 
 if (typeof window !== 'undefined') window.AIRBNB_INDIA_DATA = { destinations: DESTINATIONS, listings: LISTINGS, posts: POSTS, testimonials: TESTIMONIALS, notifications: NOTIFICATIONS };
 if (typeof module !== 'undefined' && module.exports) module.exports = { DESTINATIONS, LISTINGS, POSTS, TESTIMONIALS, NOTIFICATIONS };
